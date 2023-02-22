a4562ecba4ecc28ccf7e935dfab45ffc0162b1c2
andersonhcc <programadorandersonh@gmail.com>
2023-02-22 16:48:28 -0300

    drag and drop

--- src/animations/SecondAnimations/index.tsx
+++ src/animations/SecondAnimations/index.tsx
@@ -1,32 +1,69 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, { useAnimatedGestureHandler, useSharedValue, useAnimatedStyle, withSpring, withTiming } from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 export const SecondAnimations: React.FC = () => {
-  const moveBox = () => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
-  }
+  const panGestureEvent = useAnimatedGestureHandler({
+    onStart: (event, context: { translateX: number, translateY: number }) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+    onEnd: (event, context) => {
+        translateX.value = withTiming(0, {
+          duration: 2000,
+        });
+        translateY.value = withTiming(0,{
+          duration: 2000,
+        });
+    },
+  })
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
 
   return (
-    <Animated.View style={styles.container}>
-      <View style={styles.box} />
-    </Animated.View>
-  );
+    <View style={styles.container}>
+      <PanGestureHandler onGestureEvent={panGestureEvent}>
+        <Animated.View
+          style={[styles.square, rStyle]}
+        />
+      </PanGestureHandler>
 
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'space-around',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  box: {
-    width: 100,
+  dropzone: {
+    backgroundColor: 'red',
+  },
+  square: {
+    borderRadius: 15,
+    backgroundColor: 'pink',
     height: 100,
-    borderRadius: 50,
-    backgroundColor: 'red'
+    width: 100
   },
-
-
 });
