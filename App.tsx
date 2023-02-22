0900b51f5f4b59df8c9551fc95be9e1af24e340e
andersonhcc <programadorandersonh@gmail.com>
2023-02-22 15:03:10 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
 export default function App() {
+  const x = useSharedValue(0);
+
+  const animateBox = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: x.value * 200}]
+    }
+  })
+
+  const moveBox = () => {
+    x.value = Math.random()
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+
+    <Animated.View style={[styles.container, animateBox]}>
+      <View style={styles.box} />
+      
+    </Animated.View>
+    <Button title='Move' onPress={moveBox}/>
+
+    </GestureHandlerRootView>
+
   );
 }
 
@@ -15,6 +36,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  box: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: 'red'
+  },
+
 });
---
+++ README.md
@@ -0,0 +1,25 @@
+<p align='center'>
+  <img
+       width='400px'
+       src='https://assets-global.website-files.com/5d9bc5d562ffc2869b470941/5e1f9804b36ff7196d4b72a0_logo-react-native-tech.png'
+       alt='react native logo'
+  />
+</p>
+
+<h1 align='center'>
+  RN APP (with expo managed)
+</h1>
+<h2 align='center'> # Repositório com animações do react-native com a lib reanimated. </h2>
+
+
+
+[React-Native](https://reactnative.dev)
+  - Version ~> "0.70.5""
+
+## 🏗 Running
+
+- Pre running (Android|Ios)
+  - run `yarn install`
+
+
+
